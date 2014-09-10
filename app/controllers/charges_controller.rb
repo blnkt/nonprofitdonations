1fddf8ba81f6827c204d82c54c63df335d8545d4
Blake Mason & Ellen Krause Lehman <student@epicodus.com>
2014-09-09 17:18:10 -0700

    wip

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -6,20 +6,14 @@ class ChargesController < ApplicationController
   def create
     # Amount in cents
     @amount = 500
-
-    customer = Stripe::Customer.create(
-      :email => 'example@stripe.com',
-      :card  => params[:stripeToken]
-    )
-
-    charge = Stripe::Charge.create(
-      :customer    => customer.id,
-      :amount      => @amount,
-      :description => 'Non-Profit Donator',
-      :currency    => 'usd'
-    )
+    Customer.create(customer_params)
   rescue Stripe::CardError => e
     flash[:error] = e.message
     redirect_to charges_path
+  end
+
+private
+  def customer_params
+    params.require(:customer).permit(:stripeTokenType, :stripeToken, :stripeEmail)
   end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,4 @@
- class Charge < ActiveRecord::Base
+ class Customer < ActiveRecord::Base
   before_create :payment_details
 
 private
@@ -7,8 +7,8 @@ private
     @amount = 500
 
     customer = Stripe::Customer.create(
-      :email => 'example@stripe.com',
-      :card  => params[:stripeToken]
+      :email => self,
+      :card  => self[:stripeToken]
     )
 
     charge = Stripe::Charge.create(
@@ -17,6 +17,8 @@ private
       :description => 'Non-Profit Donator',
       :currency    => 'usd'
     )
+    self.update(email: customer[:email], card: customer[:card])
+    charge.create(customer: self.id, amount: @amount, description: charge[:description], currency: charge[:currency])
   rescue Stripe::CardError => e
     flash[:error] = e.message
     redirect_to charges_path
